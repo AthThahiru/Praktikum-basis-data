5e78446b07133179e2c492bf803555cfb7d44c50
AthThahiru <fikrie.kdsmgl@gmail.com>
2025-03-13 16:37:05 +0700

    TugasModul4_DBMS_2420506044.sql

--- TugasModul4_DBMS_2420506044.sql
+++ TugasModul4_DBMS_2420506044.sql
@@ -1,15 +1,3 @@
-C:\Users\hp>CD C:\xampp\mysql\bin
-
-C:\xampp\mysql\bin>mysql -u root  -p
-Enter password:
-Welcome to the MariaDB monitor.  Commands end with ; or \g.
-Your MariaDB connection id is 8
-Server version: 10.4.32-MariaDB mariadb.org binary distribution
-
-Copyright (c) 2000, 2018, Oracle, MariaDB Corporation Ab and others.
-
-Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
-
 MariaDB [(none)]> CREATE DATABASE toko_aththahiru;
 Query OK, 1 row affected (0.015 sec)
 
